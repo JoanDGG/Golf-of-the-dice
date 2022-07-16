375a3862a2764cd5e02498826ba245d9c0706ddc
DiegoCEspino <diegoespino930@gmail.com>
2022-07-16 13:13:41 -0700

    Die movement progression

--- Assets/Scripts/DieMovement.cs
+++ Assets/Scripts/DieMovement.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class DieMovement : MonoBehaviour
 {
+
+    private bool MouseHeld = false;
+    private Vector3 mousePosition;
+    private float StrokePower = 0;
+
+    private GameObject arrow;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        arrow = GameObject.Find("arrow");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MouseHeld = Input.GetMouseButton(0);
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        float distanceX = mousePosition.x - transform.position.x;
+        float distanceY = mousePosition.y - transform.position.y;
+        float angle = (Mathf.Atan(distanceY / distanceX) * Mathf.Rad2Deg);
+        angle = distanceX < 0 ? angle + 180 : angle; // If the mouse is behind the die add 180 degrees to compensate for flipping the arrow
+        arrow.transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+
+        if (Cursor.visible)
+        {
+            Cursor.visible = false;
+        }
+
+
+        if (MouseHeld)
+        {
+            Debug.Log("Pressed primary button.");
+        }
     }
 }
