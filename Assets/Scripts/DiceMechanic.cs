9973d6e1b6e00d4d73a6e363be78eb959cbdc9ad
OmarSorchini <omarsorchinijr@gmail.com>
2022-07-16 18:25:38 -0500

    DICE ANIMATION

--- Assets/Scripts/DiceMechanic.cs
+++ Assets/Scripts/DiceMechanic.cs
@@ -12,12 +12,17 @@ public class DiceMechanic : MonoBehaviour
 
     // Mouse release
     private bool MouseRelease = false;
+    private Rigidbody2D rb2d;
+    private float velocityX;
+    private float velocityY;
+    
 
 	// Use this for initialization
 	private void Start () {
 
         // Assign Renderer component
         rend = GetComponent<SpriteRenderer>();
+        rb2d = GetComponent<Rigidbody2D>();
 
         // Load dice sides sprites to array from Dice subfolder of Resources folder
         diceSides = Resources.LoadAll<Sprite>("Dice");
@@ -26,8 +31,10 @@ public class DiceMechanic : MonoBehaviour
 
     private void Update(){
         MouseRelease = Input.GetMouseButtonUp(0);
+        velocityX = Mathf.Abs(rb2d.velocity.x);
+        velocityY = Mathf.Abs(rb2d.velocity.y);
 
-        if (MouseRelease)
+        if (MouseRelease && velocityX < 0.1 && velocityY < 0.1)
         {
             StartCoroutine("RollTheDice");
         }
@@ -75,6 +82,6 @@ public class DiceMechanic : MonoBehaviour
 
         // Show final dice value in Console
         finalSide = Mathf.Ceil(finalSide/12f);
-        Debug.Log(finalSide);
+        Debug.Log("Dice side: " + finalSide);
     }
 }
--- Assets/Scripts/DieMovement.cs
+++ Assets/Scripts/DieMovement.cs
@@ -5,11 +5,20 @@ using UnityEngine;
 public class DieMovement : MonoBehaviour
 {
 
+    //-------------------------------------
+    // Array of dice sides sprites to load from Resources folder
+    private Sprite[] diceSides;
+
+    // Reference to sprite renderer to change sprites
+    private SpriteRenderer rend;
+    //-------------------------------------
+
     private bool MouseClick = false;
     private bool MouseHeld = false;
     private bool MouseRelease = false;
     private Vector3 mousePosition;
     public float StrokePower;
+    private int numberRolled;
 
     private GameObject arrow;
     private Rigidbody2D rb2d;
@@ -17,6 +26,14 @@ public class DieMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //-------------------------------------
+        // Assign Renderer component
+        rend = GetComponent<SpriteRenderer>();
+
+        // Load dice sides sprites to array from Dice subfolder of Resources folder
+        diceSides = Resources.LoadAll<Sprite>("Dice");
+        //-------------------------------------
+
         arrow = GameObject.Find("ArrowRotation");
         arrow.SetActive(false);
         rb2d = GetComponent<Rigidbody2D>();
@@ -65,15 +82,18 @@ public class DieMovement : MonoBehaviour
             
             arrow.SetActive(false);
             Stroke(distanceX * StrokePower, distanceY * StrokePower);
+            //-------------------------------------
+            StartCoroutine("RollTheDice");
+            //-------------------------------------
         }
     }
 
     private void Stroke(float x, float y)
     {
         rb2d.AddForce(new Vector2(x, y));
-        int numberRolled = Mathf.RoundToInt((Random.value) * 6);
+        numberRolled = Mathf.RoundToInt((Random.value) * 6);
         numberRolled = numberRolled == 0 ? 1 : numberRolled;
-        Debug.Log(numberRolled);
+        Debug.Log("DICE: " + numberRolled);
         object[] obj = GameObject.FindSceneObjectsOfType(typeof (GameObject));
         foreach (object o in obj)
         {
@@ -84,4 +104,32 @@ public class DieMovement : MonoBehaviour
             }
         }
     }
+    //--------------------------------------------------------------------------------------------------------
+    private IEnumerator RollTheDice()
+    {
+        // Variable to contain random dice side number.
+        // It needs to be assigned. Let it be 0 initially
+        int randomDiceSide = 0;
+
+        // Loop to switch dice sides ramdomly
+        // before final side appears. n itterations here.
+
+        int n = 20;
+
+        for (int i = 0; i <= n; i++)
+        {
+            // Pick up random value from 0 to 6 (All inclusive)
+            randomDiceSide = Random.Range(0, 72);   //72 sprites present with 12 variations of each side
+
+
+            // Set sprite to upper face of dice from array according to random value
+            rend.sprite = diceSides[randomDiceSide];
+
+            // Pause before next itteration
+            yield return new WaitForSeconds(0.05f);
+        }
+        rend.sprite = diceSides[numberRolled*12-1];
+
+    }
+    //--------------------------------------------------------------------------------------------------------
 }
