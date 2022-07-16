49a277e74470bf59e2383bc0f6c54696a72a40ca
DiegoCEspino <diegoespino930@gmail.com>
2022-07-16 13:41:51 -0700

    Stroke aiming 1.0

--- Assets/Scripts/DieMovement.cs
+++ Assets/Scripts/DieMovement.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class DieMovement : MonoBehaviour
 {
 
+    private bool MouseClick = false;
     private bool MouseHeld = false;
+    private bool MouseRelease = false;
     private Vector3 mousePosition;
     private float StrokePower = 0;
 
@@ -14,30 +16,46 @@ public class DieMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        arrow = GameObject.Find("arrow");
+        arrow = GameObject.Find("ArrowRotation");
+        arrow.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        MouseClick = Input.GetMouseButtonDown(0);
         MouseHeld = Input.GetMouseButton(0);
+        MouseRelease = Input.GetMouseButtonUp(0);
         mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        float distanceX = 1f; // Default value
+        float distanceY = 1f; // Default value
 
-        float distanceX = mousePosition.x - transform.position.x;
-        float distanceY = mousePosition.y - transform.position.y;
-        float angle = (Mathf.Atan(distanceY / distanceX) * Mathf.Rad2Deg);
-        angle = distanceX < 0 ? angle + 180 : angle; // If the mouse is behind the die add 180 degrees to compensate for flipping the arrow
-        arrow.transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+        if (MouseClick)
+        {
+            arrow.SetActive(true);
+        }
+
+        if (MouseHeld)
+        {
+            distanceX = mousePosition.x - arrow.transform.position.x;
+            distanceY = mousePosition.y - arrow.transform.position.y;
+            float angle = (Mathf.Atan(distanceY / distanceX) * Mathf.Rad2Deg);
+            angle = distanceX < 0 ? angle + 180 : angle;
+            arrow.transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+        }
+
+        if (MouseRelease)
+        {
+            distanceX = mousePosition.x - arrow.transform.position.x;
+            distanceY = mousePosition.y - arrow.transform.position.y;
+            StrokePower = Mathf.Sqrt((distanceX * distanceX) + (distanceY * distanceY));
+            Debug.Log("Power: " + StrokePower.ToString("F2"));
+            arrow.SetActive(false);
+        }
 
         if (Cursor.visible)
         {
             Cursor.visible = false;
-        }
-
-
-        if (MouseHeld)
-        {
-            Debug.Log("Pressed primary button.");
         }
     }
 }
