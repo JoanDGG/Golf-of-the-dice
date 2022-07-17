025de93a52453755e3c231b21277d67b61dc50ee
DiegoCEspino <diegoespino930@gmail.com>
2022-07-16 22:14:41 -0700

    Added ice

---
+++ Assets/Scripts/Ice.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ice : MonoBehaviour
+{
+    public float drag;
+
+    private void Awake()
+    {
+        gameObject.GetComponent<Collider2D>().isTrigger = true;
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<Rigidbody2D>().drag = drag;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<Rigidbody2D>().drag = 1f;
+        }
+    }
+}
