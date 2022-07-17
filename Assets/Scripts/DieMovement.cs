5bd9ee69136ff53d3e459003ed8217847127f277
DiegoCEspino <diegoespino930@gmail.com>
2022-07-16 18:25:58 -0700

    DieMovement progress

--- Assets/Scripts/DieMovement.cs
+++ Assets/Scripts/DieMovement.cs
@@ -14,13 +14,17 @@ public class DieMovement : MonoBehaviour
     private SpriteRenderer rend;
     //-------------------------------------
 
+    // Inputs
     private bool MouseClick = false;
     private bool MouseHeld = false;
     private bool MouseRelease = false;
     private Vector3 mousePosition;
+    
+    // Values
     public float StrokePower;
     private int numberRolled;
 
+    // Components
     private GameObject arrow;
     private Rigidbody2D rb2d;
 
@@ -43,41 +47,47 @@ public class DieMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Get inputs
         MouseClick = Input.GetMouseButtonDown(0);
         MouseHeld = Input.GetMouseButton(0);
         MouseRelease = Input.GetMouseButtonUp(0);
         mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
+        // Die's movement and position details
         float distanceX = mousePosition.x - arrow.transform.position.x;
         float distanceY = mousePosition.y - arrow.transform.position.y;
         float velocityX = Mathf.Abs(rb2d.velocity.x);
         float velocityY = Mathf.Abs(rb2d.velocity.y);
 
+        // Start to aim
         if (MouseClick && velocityX < 0.1 && velocityY < 0.1)
         {
             arrow.SetActive(true);
         }
 
+        // Aiming
         if (MouseHeld && velocityX < 0.1 && velocityY < 0.1)
         {
+            // Arrow's rotation according to mouse position
             float angle = (Mathf.Atan(distanceY / distanceX) * Mathf.Rad2Deg);
             angle = distanceX < 0 ? angle + 180 : angle;
             arrow.transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
 
             float distance = Mathf.Sqrt((distanceX * distanceX) + (distanceY * distanceY));
 
-            if (distance < 0.5f)
+            if (distance < 0.5f) // Minimum arrow distance
             {
                 distance = 0.5f;
             }
-            else if (distance > 2f)
+            else if (distance > 2f) // Maximum arrow distance
             {
                 distance = 2f;
             }
 
-            arrow.transform.localScale = new Vector3(distance, 1f, 1f);
+            arrow.transform.localScale = new Vector3(distance, 1f, 1f); // Change arrow's size
         }
 
+        // Strike die
         if (MouseRelease && velocityX < 0.1 && velocityY < 0.1 && arrow.activeSelf)
         {
             
@@ -91,11 +101,11 @@ public class DieMovement : MonoBehaviour
 
     private void Stroke(float x, float y)
     {
-        rb2d.AddForce(new Vector2(x, y));
-        numberRolled = Mathf.RoundToInt((Random.value) * 6);
-        numberRolled = numberRolled == 0 ? 1 : numberRolled;
+        rb2d.AddForce(new Vector2(x, y)); // Add movement
+        numberRolled = Mathf.RoundToInt((Random.value) * 6); // Random number between 0.00 and 6.00
+        numberRolled = numberRolled == 0 ? 1 : numberRolled; // If 0 set to 1
         Debug.Log("DICE: " + numberRolled);
-        StartCoroutine(ChangeEnvironment());
+        StartCoroutine(ChangeEnvironment()); // Acitvate / Deactivate obstacles
     }
     //--------------------------------------------------------------------------------------------------------
     private IEnumerator RollTheDice()
@@ -128,21 +138,23 @@ public class DieMovement : MonoBehaviour
     private IEnumerator ChangeEnvironment()
     {
         yield return new WaitForSeconds(0.5f); // Delay code execution to allow die to speed up
-        while (Mathf.Abs(rb2d.velocity.x) >= 0.1 || Mathf.Abs(rb2d.velocity.y) >= 0.1) // Wait for die to stop
+
+        // Wait for die to stop
+        while (Mathf.Abs(rb2d.velocity.x) >= 0.1 || Mathf.Abs(rb2d.velocity.y) >= 0.1)
         {
             yield return new WaitForSeconds(0.05f);
         }
 
-        // Set active obstacles according to number rolled
+        // Go through all gameobjects
         string[] tags = {"1", "2", "3", "4", "5", "6"};
         GameObject[] obj = Resources.FindObjectsOfTypeAll<GameObject>();
         foreach (GameObject g in obj)
         {
-            if (g.CompareTag(numberRolled.ToString()))
+            if (g.CompareTag(numberRolled.ToString())) // Activate obstacles according to number rolled
             {
                 g.SetActive(true);
             }
-            else if (tags.Contains(g.tag))
+            else if (tags.Contains(g.tag)) // Dectivate obstacles according to number rolled
             {
                 g.SetActive(false);
             }
