7af6d9a3db511952c4f7ac641576bf430fecdec0
DiegoCEspino <diegoespino930@gmail.com>
2022-07-17 11:52:17 -0700

    merge conflicts

--- Assets/Scripts/MapSelection.cs
+++ Assets/Scripts/MapSelection.cs
@@ -14,24 +14,10 @@ public class MapSelection : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-<<<<<<< HEAD
-        Debug.Log("Map: " + map);
-        Instantiate(maps[map], new Vector3(2.5f, -1.5f, 20), Quaternion.identity);
-        /*start = GameObject.Find("StartingPoint");
-        die = GameObject.Find("PinkDice");
-        die.transform.position = new Vector3(start.transform.position.x, start.transform.position.y, 0f);*/
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-=======
         if(SceneManager.GetActiveScene().name == "GolfingScene")
         {
             Debug.Log("Map: " + map);
             Instantiate(maps[map], new Vector3(2.5f, -1.5f, 20), Quaternion.identity);
         }
->>>>>>> f95c389fc37b6294be6ad4d2ec70f0abf591c69a
     }
 }
