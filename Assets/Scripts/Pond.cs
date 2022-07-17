8b8447437abd791851d764735b2182ea6bc324ab
DiegoCEspino <diegoespino930@gmail.com>
2022-07-17 12:05:44 -0700

    Fixed pond spawn points

--- Assets/Scripts/Pond.cs
+++ Assets/Scripts/Pond.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class Pond : MonoBehaviour
+public class Pond : MonoBehaviourPunCallbacks
 {
     private GameObject staritngPoint;
+    private Transform[] spawnPoints;
 
     private void Awake()
     {
         gameObject.GetComponent<Collider2D>().isTrigger = true;
         staritngPoint = GameObject.Find("StartingPoint");
+        spawnPoints = GameObject.Find("SpawnPlayers").GetComponent<SpawnPlayers>().spawnPoints;
     }
 
     private void OnTriggerStay2D(Collider2D other)
@@ -17,7 +20,7 @@ public class Pond : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             other.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(0f, 0f, 0f);
-            other.gameObject.transform.position = new Vector3(staritngPoint.transform.position.x, staritngPoint.transform.position.y, 0f);
+            other.gameObject.transform.position = spawnPoints[PhotonNetwork.LocalPlayer.ActorNumber - 1].position;
         }
     }
 }
